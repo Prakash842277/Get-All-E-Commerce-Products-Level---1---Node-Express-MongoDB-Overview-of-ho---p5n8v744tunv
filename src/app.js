e73703175cdcd832acf80c8df93b513dacc2bbbb
Prakash Chandra <prakash140chandra@gmail.com>
2023-04-26 13:31:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,26 @@ const app = express();
 // Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
-// Middlewares
+//Middlewares
 app.use(express.json());
+app.get("/api/v1/products/:id", (req, res)=>{
+    const product = products.find((el)=> el.id ===Number(req.params.id));
+    if(!product){
+        return res.status(404).json({
+            status: "fail",
+            message: "Product not found!",
+        });
+    }
+    res.status(200).json({
+       status: "success",
+       message: "Product fetched successfully",
+       data: {
+        product,
+       }, 
+    });
+});
 
-// Write GET endpoint for sending all the products to client here
-// Endpoint - /api/v1/products
+// GET endpoint for sending the products to client by id
+//// Endpoint - /api/v1/products/:id
 
 module.exports = app;
